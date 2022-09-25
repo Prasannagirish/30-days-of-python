e1d19c5117201d76b7e1be2ae53fef10885bb5e9
Prasannagirish <110583374+Prasannagirish@users.noreply.github.com>
2022-09-25 11:25:16 +0530

    d-3

---
+++ day-03.py
@@ -0,0 +1,35 @@
+age = 17
+height = float(177.4)
+base = input(int('Enter base:'))
+height = input(int('Enter height:'))
+area = base*height
+print(base)
+print(height)
+print(area)
+side_a = input(int('Side a :'))
+side_b = input(int('Side b :'))
+side_c = input(int('Side c :'))
+perimeter = side_a+side_b+side_c
+print(side_a,side_b,side_c)
+print(perimeter)
+print('I hope this course is not full of jargon', 'jargon' in ' I hope this course is not full of jargon')
+x1 = input(int('x1 :'))
+y1 = input(int('y1 :'))
+x2 = input(int('x2 :'))
+y2 = input(int('y2 :'))
+slope = y2-y1/x2-x1
+print(slope)
+str_name = 'Python'
+len_str_name = len(str_name)
+float_name = float(len_str_name)
+string_name = str(float_name)
+print(string_name)
+val = int(9.8)
+print(val == 10)
+div = 7//3
+int_div = int(div)
+x = 2.7
+y = int(x)
+print(int_div == y)
+
+
