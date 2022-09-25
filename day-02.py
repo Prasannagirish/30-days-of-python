12d1dcca618f65bd622931c8451399e9785f6a42
Prasannagirish <110583374+Prasannagirish@users.noreply.github.com>
2022-09-25 10:52:45 +0530

    d-2

---
+++ day-02.py
@@ -0,0 +1,54 @@
+
+first_name = 'Girisj'
+last_name = 'Prasanna'
+country = 'India'
+city = 'Chennai'
+age = 17
+is_married = False
+skills = ['Badminton', 'Cricket', 'Football', 'Music', 'Python']
+person_info = {
+   'firstname':'Girish',
+   'lastname':'Prasanna',
+   'country':'India',
+   'city':'Chennai'
+   }
+
+
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
+
+
+
+num_int = 10
+print('num_int',num_int)         
+num_float = float(num_int)
+print('num_float:', num_float)   
+
+gravity = 9.81
+print(int(gravity))            
+
+
+num_int = 10
+print(num_int)                  
+num_str = str(num_int)
+print(num_str)                  
+
+
+num_str = '10.6'
+print('num_int', int(num_str))     
+print('num_float', float(num_str))  
+
+
+first_name = 'Girish'
+print(first_name)             
+first_name_to_list = list(first_name)
+print(first_name_to_list)           
